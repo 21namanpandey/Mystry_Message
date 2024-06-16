3f38664f76fa84c36fd06c993e1a3db471a42937
Naman Pandey <2492004namanpandey@gmail.com>
2024-06-16 21:02:28 +0530

    fix max length error in username in sign up schema

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -3,7 +3,7 @@ import {z} from 'zod'
 export const usernameValidation = z
     .string()
     .min(2, "Username must be at least 2 character")
-    .min(20, "Username must be no more than 20 character")
+    .max(20, "Username must be no more than 20 character")
     .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special character")
 
 export const signUpSchema = z.object({
